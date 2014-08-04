798785abc5981cbc1182c228c918220ccb802456
Tennessee Leeuwenburg <t.leeuwenburg@bom.gov.au>
2014-08-04 10:07:07 +1000

    Added some more files just for structure

--- tests/test_matplotlib.py
+++ tests/test_matplotlib.py
@@ -1,4 +1,31 @@
 
-test_basic_matplotlib():
+import matplotlib
+import wordgraph
 
-    assert False
+
+def test_basic_matplotlib():
+
+    """
+    Based on the demo at: http://matplotlib.org/examples/lines_bars_and_markers/barh_demo.html
+    Viewed on 4 August 2014
+
+    Simple demo of a horizontal bar chart.
+    """
+    import matplotlib.pyplot as plt; plt.rcdefaults()
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    # Example data
+    people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
+    y_pos = np.arange(len(people))
+    performance = 3 + 10 * np.random.rand(len(people))
+    error = np.random.rand(len(people))
+
+    plt.barh(y_pos, performance, xerr=error, align='center', alpha=0.4)
+    plt.yticks(y_pos, people)
+    plt.xlabel('Performance')
+    plt.title('How fast do you want to go today?')
+
+    text = wordgraph.describe(plt, source='matplotlib')
+    assert text is not None
+    
