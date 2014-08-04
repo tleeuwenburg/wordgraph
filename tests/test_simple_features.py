893a611290e221f9fdc226ba7482b402ffa0cfab
Aaron Iles <aaron.iles@gmail.com>
2014-08-04 10:29:02 +1000

    Factor out timing of points sequence
    Create generic function for adding time values to a series of data
    points.

--- tests/test_simple_features.py
+++ tests/test_simple_features.py
@@ -1,10 +1,15 @@
+from wordgraph.points import Point
 import wordgraph
 
 EPOCH_START = 1407109280
 
-def test_monotonic_up_per_second():
+def time_values(values, start=EPOCH_START, increment=1):
     datapoints = []
-    for i in range(10):
-        datapoints.append((float(i), EPOCH_START + i))
-    features = wordgraph.name_things(datapoints)
+    for index, value in enumerate(values):
+        datapoints.append(Point(x=value, y=start + (increment * index)))
+    return datapoints
+
+def test_monotonic_up_per_second():
+    datapoints = time_values(float(i) for i in range(POINTS))
+    features = wordgraph.describe(datapoints)
     assert "" in features
