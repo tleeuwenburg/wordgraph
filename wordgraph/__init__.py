0760ae2912e9dbb2e8e4faa8c801f7e87e5c3eb7
Tennessee Leeuwenburg <tleeuwenburg@gmail.com>
2014-08-04 10:07:07 +1000

    Added some more files just for structure

---
+++ wordgraph/__init__.py
@@ -0,0 +1,3 @@
+
+
+from describer import describe
---
+++ wordgraph/describer.py
@@ -0,0 +1,23 @@
+
+
+def describe(object, source=None):
+    '''
+    Describe the supplied graph object, together with a hint about the source of that object.
+
+    @return: None if there was no description text generated for the graph
+    @return: 
+
+    Supported sources include:
+      -- Raw data
+      -- matplotlib (under dev)
+
+
+    Unsupported sources include:
+      -- graphite (under dev)
+      -- dot, networkx?
+      -- json, text?
+
+
+    '''
+
+    return None
