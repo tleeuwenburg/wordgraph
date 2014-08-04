b3531ab89b9795903e595c0f3f9fb20417958414
Aaron Iles <aaron.iles@gmail.com>
2014-08-04 10:31:34 +1000

    Test case for monotonically decreasing graphs
    Generate time series data for values that decrease monotonically over
    time.

--- tests/test_simple_features.py
+++ tests/test_simple_features.py
@@ -10,6 +10,11 @@ def time_values(values, start=EPOCH_START, increment=1):
     return datapoints
 
 def test_monotonic_up_per_second():
-    datapoints = time_values(float(i) for i in range(POINTS))
+    datapoints = time_values(float(i) for i in range(10))
+    features = wordgraph.describe(datapoints)
+    assert "" in features
+
+def test_monotonic_down_per_second():
+    datapoints = time_values(10.0 - i for i in range(10))
     features = wordgraph.describe(datapoints)
     assert "" in features
