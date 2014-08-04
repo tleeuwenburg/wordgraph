bc4fb65f76aa011e44bbe01b7965bc99eff5d85e
Aaron Iles <aaron.iles@gmail.com>
2014-08-04 11:55:47 +1000

    Test expected failures of the anlayzer
    The analyzer is not expected to cope with too few data points for time
    series with greatly varying time ranges. It should raise an exception in
    these cases.

--- tests/test_recalcitrant.py
+++ tests/test_recalcitrant.py
@@ -3,6 +3,7 @@ from wordgraph.points import Point
 import wordgraph
 
 import random
+import pytest
 
 from utilities import EPOCH_START, time_values
 
@@ -21,3 +22,21 @@ def test_random_data():
     datapoints = time_values(values)
     features = wordgraph.describe(datapoints)
     assert features is None
+
+def test_too_few_points():
+    """A time series with too few data points to be analysed.
+
+    Expected to raise an exception.
+    """
+    with pytest.raises(ValueError):
+        features = wordgraph.describe([Point(x=0, y=0)])
+
+def test_nonuniform_time_periods():
+    """A time series where time periods are wildly different.
+
+    Expected to raise an exception.
+    """
+    times = [1, 3, 4, 6, 7, 9, 10]
+    datapoints = [Point(x=t, y=1.0) for t in times]
+    with pytest.raises(ValueError):
+        features = wordgraph.describe(datapoints)
