532e650fb8f61592659f1f8ce282f53fe026d3be
Aaron Iles <aaron.iles@gmail.com>
2014-08-04 11:37:54 +1000

    Documentation for test cases
    Human readable descriptions of the time series being tested.

--- tests/test_recalcitrant.py
+++ tests/test_recalcitrant.py
@@ -1,3 +1,4 @@
+"Test for recalcitrant and obtuse graphs to describe"
 from wordgraph.points import Point
 import wordgraph
 
@@ -6,6 +7,7 @@ import random
 from utilities import EPOCH_START, time_values
 
 def test_time_goes_backwards():
+    "A valid time series where time changes linearly backwards"
     values = [1.0] * 10
     times = (EPOCH_START-i for i in range(10))
     datapoints = [Point(x=t, y=v) for (v, t) in zip(values, time)]
@@ -13,6 +15,7 @@ def test_time_goes_backwards():
     assert features is None
 
 def test_random_data():
+    "A time series of 50 data points where every value is random"
     rng = random.Random(0)
     values = [rng.random() for i in range(50)]
     datapoints = time_values(values)
--- tests/test_simple_features.py
+++ tests/test_simple_features.py
@@ -1,23 +1,33 @@
+"Test for artificially generated and perfect examples of graph features"
 import wordgraph
 
 from utilities import time_values
 
 def test_linear_values():
+    "Postiive constant value time series in 10, one second increments"
     datapoints = time_values(3.0 for i in range(10))
     features = wordgraph.describe(datapoints)
     assert "" in features
 
 def test_monotonic_up_per_second():
+    "Monotonically increasing positive series in 10, one second increments"
     datapoints = time_values(float(i) for i in range(10))
     features = wordgraph.describe(datapoints)
     assert "" in features
 
 def test_monotonic_down_per_second():
+    "Monotonically descreasing positive series in 10, one second increments"
     datapoints = time_values(10.0 - i for i in range(10))
     features = wordgraph.describe(datapoints)
     assert "" in features
 
 def test_tent_map():
+    """Tent map time series
+    
+    A 20 second time series in two distinct, 10 second ranges with one second
+    increments. First period is monotonically increasing. The second period is
+    monotonically decreasing at the same rate.
+    """
     values = [float(i) for i in range(10)]
     values.append(11.0)
     values += [10.0 - i for i in range(10)]
@@ -26,6 +36,12 @@ def test_tent_map():
     assert "" in features
 
 def test_step_function():
+    """Step function time series
+
+    A 20 second time series in two distinct, 10 second ranges with one second
+    increments. First period is a positive constant value. The second period is
+    a different, positive constant value.
+    """
     values = [1.0] * 10
     values += [2.0] * 10
     datapoints = time_values(values)
@@ -33,6 +49,12 @@ def test_step_function():
     assert "" in features
 
 def test_saw_tooth():
+    """A saw tooth time series
+
+    A 25 second time series of 5 distinct periods. Each period exhibits the
+    same monotonically increasing positive series. The series resets to the
+    initial state on each section boundary.
+    """
     values = [1.0 + i for i in range(5)]
     values *= 5
     values.append(1.0)
