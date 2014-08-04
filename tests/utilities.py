5b7c1373eba71580f4fc0e6046c9a8639b904a6b
Aaron Iles <aaron.iles@gmail.com>
2014-08-04 14:57:56 +1000

    Correctly assign time to x axis
    The utility function was incorrectly assigning time to the y axis and
    the value to the x axis.

--- tests/utilities.py
+++ tests/utilities.py
@@ -19,5 +19,5 @@ EPOCH_START = 1407109280
 def time_values(values, start=EPOCH_START, increment=1):
     datapoints = []
     for index, value in enumerate(values):
-        datapoints.append(Point(x=value, y=start + (increment * index)))
+        datapoints.append(Point(y=value, x=start + (increment * index)))
     return datapoints
