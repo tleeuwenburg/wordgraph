ac37e433cc3357aa2c9eedcc04f71ed9d891e840
HawkOwl <hawkowl@atleastfornow.net>
2014-08-05 15:32:39 +1000

    Fix up setup so that it installs right

--- wordgraph/__init__.py
+++ wordgraph/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .describer import describe
+from wordgraph.describer import describe
